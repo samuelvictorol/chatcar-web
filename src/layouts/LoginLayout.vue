292e2a4e320e9eb60dd3db65c587b650304c9df8
samuel.victor <samuel.victor@ebserh.gov.br>
2025-07-29 13:06:49 -0300

    fix toggle registrar

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -77,7 +77,7 @@
                             <q-btn v-if="!loading" :label="formType === 'login' ? 'Entrar' : 'Registrar'"
                                 type="submit" color="teal" glossy class="w100 q-mt-md q-pa-md" no-caps />
                             <q-btn v-if="!loading" flat dense no-caps color="teal" class="w100 q-mt-sm text-bold"
-                                @click="formType = 'register'">
+                                @click="formType = formType === 'login' ? 'register' : 'login'">
                                 {{ formType === 'login' ? 'Ainda não tem conta? Cadastre-se' : 'Já tem conta? Entrar' }}
                             </q-btn>
                             <!-- <q-btn v-if="!loading" flat dense no-caps color="teal" class="w100 q-mt-sm text-bold"
